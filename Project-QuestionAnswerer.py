4edc3f7bf3ed23b5e7877df2c118f7e9a83bec91
Nidaige <henky95@gmail.com>
2021-11-15 10:59:02 +0100

    Created dataloader for jsonlines files

--- Project-QuestionAnswerer.py
+++ Project-QuestionAnswerer.py
@@ -1,31 +1,25 @@
-''' Project description
-Full description from canvas:
-    Task: Questioning answering
-    Data: https://paperswithcode.com/dataset/natural-questions
-    Start with yes/no, continue with complex answers
-
-About the data:
-    - 42 GB in size - Really F*ing large! Is downloading the best option? or is there a way to fetch from code?
-
-    - The Natural Questions corpus is a question answering dataset containing 307,373 training examples,
-    7,830 development examples, and 7,842 test examples.
-
-    - Each example is comprised of a google.com query and a corresponding Wikipedia page.
-
-    - Each Wikipedia page has a passage (or long answer) annotated on the page that answers the question and one or more
-    short spans from the annotated passage containing the actual answer.
-
-    - The long and the short answer annotations can however be empty.
-
-    - If they are both empty, then there is no answer on the page at all.
-
-    - If the long answer annotation is non-empty, but the short answer annotation is empty, then the annotated passage
-    answers the question but no  explicit short answer could be found.
-
-    - Finally 1% of the documents have a passage annotated with a short answer that is “yes” or “no”,
-    instead of a list of short spans.
+import jsonlines as jsonlines
 
 
-How data is represented:
-    -
-'''
+class NQDataLoader():  # Data loader class for Natural Questions
+    def __init__(self):
+        self.yesNoQuestions = {}  # Holds all objects with yes-no-answer
+        with jsonlines.open('Data/Project/v1.0-simplified_nq-dev-all.jsonl') as reader:  # open file
+            Id = 0
+            for obj in reader:
+                if obj["annotations"][-1]["yes_no_answer"] != "NONE":  # If the question has a yes/no-answer
+                    self.yesNoQuestions[Id] = (obj)  # Add it to the dict
+                    Id += 1  # Increment ID
+                    if Id == 20:  # stop after 20 questions are found, remove when model works
+                        break
+
+    def __getitem__(self, idx):
+        return self.yesNoQuestions[idx]
+
+    def __len__(self):
+        return(len(self.yesNoQuestions))
+
+
+
+NQDataset = NQDataLoader()
+print(NQDataset.__getitem__(11)["annotations"][-1]["yes_no_answer"])
