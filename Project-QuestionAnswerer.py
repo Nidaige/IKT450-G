fa405ef51a99a67f6d77c3eb51a383837e3476eb
Nidaige <henky95@gmail.com>
2021-11-15 11:40:40 +0100

    fixed monday morning blunder :100: :eggplant: :sweat_drops:

--- Project-QuestionAnswerer.py
+++ Project-QuestionAnswerer.py
@@ -3,12 +3,12 @@ import jsonlines as jsonlines
 
 class NQDataLoader():  # Data loader class for Natural Questions
     def __init__(self):
-        self.yesNoQuestions = {}  # Holds all objects with yes-no-answer
+        self.yesNoQuestions = []  # Holds all objects with yes-no-answer
         with jsonlines.open('Data/Project/v1.0-simplified_nq-dev-all.jsonl') as reader:  # open file
             Id = 0
             for obj in reader:
                 if obj["annotations"][-1]["yes_no_answer"] != "NONE":  # If the question has a yes/no-answer
-                    self.yesNoQuestions[Id] = (obj)  # Add it to the dict
+                    self.yesNoQuestions.append(obj)  # Add it to the dict
                     Id += 1  # Increment ID
                     if Id == 20:  # stop after 20 questions are found, remove when model works
                         break
